d9cec8a00116650eaad40b26fadf5567efe878e0
Jojo-Supawit <supawitk.work@gmail.com>
2026-09-04 22:47:59 +0700

    fix: change test api to use clerk user id from .env

--- apps/api/src/testAPI/test.service.ts
+++ apps/api/src/testAPI/test.service.ts
@@ -6,7 +6,10 @@ export class TestService {
     constructor(@Inject('CLERK_CLIENT') private readonly clerkClient: ClerkClient) {}
 
     async getTestToken(): Promise<string> {
-        const sessions = await this.clerkClient.sessions.createSession({userId: "user_3Io8Nz2zdL3A6rE9WP3G2NQtCBB"});
+        if(!process.env['TEST_CLERK_USER_ID']){
+            return '';
+        }
+        const sessions = await this.clerkClient.sessions.createSession({userId: process.env['TEST_CLERK_USER_ID']});
         const token = await this.clerkClient.sessions.getToken(sessions.id);
         return token.jwt;
     }
