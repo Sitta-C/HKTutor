f41dd620cae6c26bfc9482e1d750f097e792445e
unknown <peerapatv.wong@gmail.com>
2026-09-05 23:55:52 +0700

    feat: enable CORS

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,14 @@ import { configureApplication } from './app.setup';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // only if you're using cookies; not needed for Bearer-token auth
+  });
+
   configureApplication(app);
   await app.listen(process.env['PORT'] ?? 3001);
 }
