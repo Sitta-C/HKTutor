278f389cfd5b2b729cc570243193372f66cf2170
Jojo-Supawit <supawitk.work@gmail.com>
2026-09-06 16:24:30 +0700

    fix: edit import group and order

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 
 import { AppService } from '@/app.service';
-
 import { ClerkAuthGuard } from '@/auth/auth.guard';
 
 @Controller()
--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,13 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
+import { AuthModule } from '@/auth/auth.module';
 import { validateDatabaseEnvironment } from '@/config/database.config';
 import { DatabaseModule } from '@/database/database.module';
 import { HealthModule } from '@/health/health.module';
 import { TestModule } from '@/testAPI/test.module';
 import { TutorsModule } from '@/tutors/tutors.module';
-
-import { AuthModule } from '@/auth/auth.module';
 
 @Module({
   imports: [
