4f07c7d425d509737dbfe8da3a50b3110ade1aed
Jojo-Supawit <supawitk.work@gmail.com>
2026-09-06 17:58:42 +0700

    fix: add UsersModule to AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -9,6 +9,7 @@ import { DatabaseModule } from '@/database/database.module';
 import { HealthModule } from '@/health/health.module';
 import { TestModule } from '@/testAPI/test.module';
 import { TutorsModule } from '@/tutors/tutors.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { TutorsModule } from '@/tutors/tutors.module';
     DatabaseModule,
     HealthModule,
     TutorsModule,
+    UsersModule,
     AuthModule,
     TestModule,
   ],
