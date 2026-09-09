c623c576d538f3ff42ea1cef374ddfdde8573f0b
Jojo-Supawit <supawitk.work@gmail.com>
2026-09-09 13:10:46 +0700

    fix: add guard to tutor controller

--- apps/api/src/tutors/tutors.controller.ts
+++ apps/api/src/tutors/tutors.controller.ts
@@ -1,23 +1,18 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
+import { JwtAuthGuard } from '@/auth/auth.guard';
 import { RolesGuard } from '@/auth/roles.guard';
 import { Roles } from '@/auth/roles.decorator';
 import { SearchTutorsQueryDto, TutorResponseDto, TutorProfileResponseDto } from '@/tutors/tutors.dto';
 import { TutorsService } from '@/tutors/tutors.service';
-import { GetTutorsDoc } from '@/tutors/tutors.swagger';
 import { GetUser } from '@/user/get-user.decorator';
 
 @ApiTags('tutors')
 @Controller('api/tutors')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class TutorsController {
   constructor(private readonly tutorsService: TutorsService) {}
-
-  @Get()
-  @GetTutorsDoc()
-  async search(@Query() query: SearchTutorsQueryDto): Promise<TutorResponseDto[]> {
-    return this.tutorsService.search(query);
-  }
 
   @Get('me/profile')
   @Roles('TUTOR')
